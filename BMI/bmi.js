21bb5d512c1f1be156ba04e4791e4fb09b877af8
vansh-verma-dev <vanshverma6065@gmail.com>
2026-02-14 23:11:50 +0530

    initial add BMI calculater staructure

--- BMI/bmi.js
+++ BMI/bmi.js
@@ -1,3 +1,20 @@
-const height = document.getElementById('height');
-const width = document.getElementById('width');
+document.getElementById('sumitbtn').addEventListener('click',(e)=>{
+
+    e.preventDefault();
+
+  const height = parseInt(document.getElementById('height').value)
+const Weight = document.getElementById('Weight').value;
 const result = document.getElementById('Result');
+
+if(height === "" || height < 0 || isNaN(height)){
+result.innerHTML = "please give a valid height"
+}else if(Weight === "" || Weight < 0 || isNaN(Weight)){
+result.innerHTML = "please give a valid Weight"
+} else{
+    const bmi = (Weight / ((height * height) / 10000)).toFixed(2);
+    result.innerHTML = `${bmi}`
+}
+})
+   
+
+ 
